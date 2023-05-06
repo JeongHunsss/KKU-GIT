3222dd9ba33518ad9c79d9e32bfcbb791f952476
JeongHunPark <mizz1030@naver.com>
2023-05-07 02:52:07 +0900

    dbname kku_git으로 변경

--- config/dbconfig.php
+++ config/dbconfig.php
@@ -3,7 +3,7 @@
 $host = 'localhost';
 $username = '';   // 본인 데이터베이스 이름
 $password = '';   // 본인 데이터베이스 비밀번호
-$dbname = 'kku-git';
+$dbname = 'kku_git';
 $conn = mysqli_connect($host, $username, $password, $dbname);
 if (!$conn) {
     die('Could not connect to the database: ' . mysqli_connect_error());
