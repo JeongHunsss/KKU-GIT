02bf2a35c6e2ab8f7ab665e3f29ab31fe8c1a583
yoolim <db12fla@naver.com>
2023-05-13 14:38:31 +0900

    수정1

---
+++ login/quizdetail.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>문제 풀기 페이지</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="./css/quizdetail.css">
+    <link rel="stylesheet" type="text/css" href="./css/quizregister.css">
+</head>
+<body>
+    <h1>문제 풀기 페이지</h1>
+    <div class="container">
+        <div class="problem-header">
+            <h2>제목: ~~~~~~~</h2>
+        </div>
+        <br>
+        <span>등록자: 한유림 | 등록일자: 2022-05-10</span>
+        <div class="problem-body">
+            <div class="problem-content">
+                <h3>문제 내용</h3>
+            </div>
+            <div class="problem-choices">
+                <h3>보기</h3>
+                <form>
+                    <label> 1. 1번 정답 내용</label>
+                    <label> 2. 2번 정답 내용</label>
+                    <label> 3. 3번 정답 내용</label>
+                    <br>
+                    <label>정답 입력: <input type="text"></label>
+                </form>
+            </div>
+            <div class="problem-buttons">
+            <button>정답 제출</button>
+            <button>정답 공개</button>
+        </div>
+    </div>
+</div>
+</body>
+</html>
---
+++ login/quizregister.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>문제 등록 페이지</title>
+		 <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="./css/quizregister.css">
+</head>
+<body>
+	<h1>문제 등록</h1>
+	<form method="post" action="submit.php">
+
+		<label for="subject">과목 <input type="text" name="subject" id="subject" required><br></label><br>
+
+		<label for="title">제목 <input type="text" name="title" id="title" required><br></label><br>
+		
+		<label for="content">내용 <textarea name="content" id="content" required></textarea><br><br></label>
+		
+		<label for="options">보기
+		1. <input type="text" name="option1" id="option1" required>
+		2. <input type="text" name="option2" id="option2" required>
+		3. <input type="text" name="option3" id="option3" required><br></label><br>
+		
+		<label for="answer">정답
+		<input type="text" name="answer" id="answer" required></label><br>
+		
+		<input type="button" value="취소" onclick="history.back();">
+		<input type="submit" value="등록">
+	</form>
+</body>
+</html>
