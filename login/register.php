93a7dca0155425d9e98aacabc807ee6d3846d08c
JeongHunPark <mizz1030@naver.com>
2023-05-19 23:53:39 +0900

    이메일 인증 코드 수정

--- login/register.php
+++ login/register.php
@@ -1,9 +1,9 @@
 <?php
 
-if (isset($_POST['register'])) {
+if (isset($_POST['regist'])) {
     $user_id = $_POST['user_id'];
     $password = $_POST['password'];
-    $name = $_POST['name'];
+    $name = $_POST['user_name'];
 
     include '../config/dbconfig.php';
 
@@ -15,6 +15,13 @@ if (isset($_POST['register'])) {
     header("Location: login.php");
     exit(); // 페이지 이동 후 코드가 실행되는 것을 방지하기 위해 exit() 함수 사용
 }
+
+if (isset($_POST['email_send'])) {
+    $receiver_email = $_POST['email'].'@kku.ac.kr';
+    $code = rand(10000, 99999);
+    include '../config/mailerconfig.php';
+}
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -26,36 +33,36 @@ if (isset($_POST['register'])) {
 <body class="join-body">
     <form method="post" class="join-form">
         <h2>KKU-GIT 회원가입</h2>
-
+    
         <label class="join-label" for="user_name">
-            이름 <input class="join-input" type="text" id="user_name" name="user_name" required>
+            이름 <input class="join-input" type="text" id="user_name" name="user_name">
        </label>
 
         <label class="join-label" for="user_id">
         <div class="input-group">
-            아이디 <input class="join-input" type="text" id="user_id" name="user_id" required>
+            아이디 <input class="join-input" type="text" id="user_id" name="user_id">
             <button class="verification-code-button">중복확인</button>
         </div></label>
 
         <br>
         <label class="join-label" for="password">
-            비밀번호 <input class="join-input" type="password" id="password" name="password" required>
+            비밀번호 <input class="join-input" type="password" id="password" name="password">
         <br></label>
 
         <label class="join-label" for="email">
         <div class="input-group">
-            이메일 <input class="join-input" type="email" id="email" name="email" required>
-            <button class="verification-code-button">인증코드 보내기</button>
+            이메일 <input class="join-input" type="text" id="email" name="email" required>@kku.ac.kr
+            <button class="verification-code-button" name = "email_send">인증코드 보내기</button>
         </div></label>
         <br>
         <label class="join-label" for="verification_code">
         <div class="input-group">
-            인증코드 <input class="join-input" type="text" id="verification_code" name="verification_code" required>
+            인증코드 <input class="join-input" type="text" id="verification_code" name="verification_code">
             <button class="verification-check-button">확인</button>
         </div></label>
         <br>
         <div class="button-group">
-            <button class="duplicate-check-button" type="submit" name="join">회원가입</button>
+            <button class="duplicate-check-button" type="submit" name="regist">회원가입</button>
             <div class="button-space"></div>
             <button class="duplicate-check-button" onclick="location.href='login.php'">취소</button>
         </div>
