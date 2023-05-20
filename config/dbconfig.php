1702252c4cde8608d98076d4d00d91694384e654
JeongHunPark <mizz1030@naver.com>
2023-05-20 23:03:20 +0900

    db 설정

--- config/dbconfig.php
+++ config/dbconfig.php
@@ -1,8 +1,8 @@
 <?php
 // 데이터베이스 연결 설정
 $host = 'localhost';
-$username = '';   // 본인 데이터베이스 이름
-$password = '';  // 본인 데이터베이스 비밀번호
+$username = 'root';   // 본인 데이터베이스 이름
+$password = '1234';  // 본인 데이터베이스 비밀번호
 $dbname = 'kku_git';
 $conn = mysqli_connect($host, $username, $password, $dbname);
 if (!$conn) {
