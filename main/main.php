28031c48dfd90d45364bc25c42121abe33070c28
yoolim <db12fla@naver.com>
2023-05-22 02:32:07 +0900

    수정미완성

--- main/main.php
+++ main/main.php
@@ -1,134 +1,134 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <h1>메인 페이지</h1>
+    <h1>KKU-GIT</h1>
     <meta charset="UTF-8">
     <link rel="stylesheet" type="text/css" href="../bars/css/side_bar.css">
     <link rel="stylesheet" type="text/css" href="../bars/css/top_bar.css">
-    <link rel="stylesheet" type="text/css" href="./css/main.css">
+    <link rel="stylesheet" type="text/css" href="./css/realmain.css">
+
 </head>
 <body>
-    <h1>문제 풀기 페이지</h1>
     <div class="notice-wrapper">
-    <div class="notice-box">
-        <h2>공지사항</h2>
-        <table class="notice-table">
-            <thead>
-                <tr>
-                    <th>번호</th>
-                    <th>제목</th>
-                    <th>등록일자</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>공지사항 제목 1</td>
-                    <td>2023-05-13</td>
-                </tr>
-                <tr>
-                    <td>2</td>
-                    <td>공지사항 제목 2</td>
-                    <td>2023-05-12</td>
-                </tr>
-                <tr>
-                    <td>3</td>
-                    <td>공지사항 제목 3</td>
-                    <td>2023-05-11</td>
-                </tr>
-                <tr>
-                    <th colspan="3" class="notice-content-title">내용</th>
-                </tr>
-                <tr>
-                    <td colspan="3">공지사항 내용 1</td>
-                </tr>
-                <tr>
-                    <td colspan="3">공지사항 내용 2</td>
-                </tr>
-                <tr>
-                    <td colspan="3">공지사항 내용 3</td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
+        <div class="notice-box">
+            <h2>공지사항</h2>
+            <table class="notice-table">
+                <thead>
+                    <tr>
+                        <th>번호</th>
+                        <th>제목</th>
+                        <th>등록일자</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>공지사항 제목 1</td>
+                        <td>2023-05-13</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>공지사항 제목 2</td>
+                        <td>2023-05-12</td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>공지사항 제목 3</td>
+                        <td>2023-05-11</td>
+                    </tr>
+                    <tr>
+                        <th colspan="3" class="notice-content-title">내용</th>
+                    </tr>
+                    <tr>
+                        <td colspan="3">공지사항 내용 1</td>
+                    </tr>
+                    <tr>
+                        <td colspan="3">공지사항 내용 2</td>
+                    </tr>
+                    <tr>
+                        <td colspan="3">공지사항 내용 3</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
 
-    <div class="notice-box">
-        <h2>랭킹</h2>
-        <table class="ranking-table">
-            <thead>
-                <tr>
-                    <th>순위</th>
-                    <th>이름(등급)</th>
-                    <th>포인트</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>유저1 (골드)</td>
-                    <td>1000</td>
-                </tr>
-                <tr>
-                    <td>2</td>
-                    <td>유저2 (실버)</td>
-                    <td>900</td>
-                </tr>
-                <tr>
-                    <td>3</td>
-                    <td>유저3 (브론즈)</td>
-                    <td>800</td>
-                </tr>
-                <tr>
-                    <td>4</td>
-                    <td>유저4 (브론즈)</td>
-                    <td>700</td>
-                </tr>
-                <tr>
-                    <td>5</td>
-                    <td>유저5 (브론즈)</td>
-                    <td>600</td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
+        <div class="notice-box ranking-box">
+            <h2>랭킹</h2>
+            <table class="ranking-table">
+                <thead>
+                    <tr>
+                        <th>순위</th>
+                        <th>이름(등급)</th>
+                        <th>포인트</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>  유저1 (골드)</td>
+                        <td>1000</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>  유저2 (실버)</td>
+                        <td>900</td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>  유저3 (브론즈)</td>
+                        <td>800</td>
+                    </tr>
+                    <tr>
+                        <td>4</td>
+                        <td>  유저4 (브론즈)</td>
+                        <td>700</td>
+                    </tr>
+                    <tr>
+                        <td>5</td>
+                        <td>유저5 (브론즈)</td>
+                        <td>600</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
 
-    <div class="notice-box">
-        <h2>최근 등록 문제</h2>
-        <table class="recent-problem-table">
-            <thead>
-            <tr>
-                    <th>등록번호</th>
-                    <th>과목</th>
-                    <th>제목</th>
-                    <th>등록자</th>
-                    <th>등록일자</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>수학</td>
-                    <td>최근 등록 문제 제목 1</td>
-                    <td>작성자1</td>
-                    <td>2023-05-13</td>
-                </tr>
-                <tr>
-                    <td>2</td>
-                    <td>영어</td>
-                    <td>최근 등록 문제 제목 2</td>
-                    <td>작성자2</td>
-                    <td>2023-05-13</td>
-                </tr>
-                <tr>
-                    <td>3</td>
-                    <td>과학</td>
-                    <td>최근 등록 문제 제목 3</td>
-                    <td>작성자3</td>
-                    <td>2023-05-12</td>
-                </tr>
-            </tbody>
-        </table>
+        <div class="notice-box recent-problem-box">
+            <h2>최근 등록 문제</h2>
+            <table class="recent-problem-table">
+                <thead>
+                    <tr>
+                        <th>번호</th>
+                        <th>과목</th>
+                        <th>제목</th>
+                        <th>등록자</th>
+                        <th>등록일자</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>수학</td>
+                        <td>최근 등록 문제 제목 1</td>
+                        <td>작성자1</td>
+                        <td>2023-05-13</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>영어</td>
+                        <td>최근 등록 문제 제목 2</td>
+                        <td>작성자2</td>
+                        <td>2023-05-13</td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>과학</td>
+                        <td>최근 등록 문제 제목 3</td>
+                        <td>작성자3</td>
+                        <td>2023-05-12</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
-</div>
 </body>
 </html>
--- problem/problem_detail.php
+++ problem/problem_detail.php
@@ -1,52 +1,55 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>문제 풀기 페이지</title>
+<title>문제 풀기 페이지</title>
     <meta charset="UTF-8">
-    <link rel="stylesheet" type="text/css" href="../bars/css/side_bar.css">
-    <link rel="stylesheet" type="text/css" href="../bars/css/top_bar.css">
-    <link rel="stylesheet" type="text/css" href="./css/problem_detail.css">
-    <link rel="stylesheet" type="text/css" href="./css/problem_add.css">
+    <link rel="stylesheet" type="text/css" href="./css/detail.css">
+    <link rel="stylesheet" type="text/css" href="./css/quizregister.css">
 </head>
 <body>
+    
     <div class="container">
-        <h1>문제 풀기 페이지</h1>
+    <br><h3>문제 풀기 페이지</h3>
         <div class="problem-header">
-            <h2>제목: ~~~~~~~</h2>
+            <h2> 문제 제목: ~~~~~~~</h2>
+            <span> 등록자: 한유림 | 등록일자: 2022-05-10</span>
         </div>
         <br>
-        <span>등록자: 한유림 | 등록일자: 2022-05-10</span><br><br>
+       
         <div class="problem-body">
             <div class="problem-content">
-                <h3>문제 내용</h3><br><br><br><br>
+                <h3> 문제 내용</h3>
             </div>
             <div class="problem-choices">
                 <h3>보기</h3>
                 <form>
-                    <label> 1. 1번 정답 내용</label>
-                    <label> 2. 2번 정답 내용</label>
-                    <label> 3. 3번 정답 내용</label>
-                    <br>
+                    <label> 1. 1번 정답 내용</label><br>
+                    <label> 2. 2번 정답 내용</label><br>
+                    <label> 3. 3번 정답 내용</label><br><br><br>
+                 
                     <label>정답 입력: <input type="text"></label>
                 </form>
-            </div>
+            
             <div class="problem-buttons">
                 <button>정답 제출</button>
                 <button>정답 공개</button>
             </div>
-        </div>
+       
 
-        <br><br><br><br><br><br>
+     
         <hr>
         <div class="comment-section">
+            
             <h3>댓글</h3>
+            
+           
             <form>
                 <label>댓글 내용</label>
                 <br>
                 <textarea rows="4" cols="50"></textarea>
-                <button type="submit">댓글 등록</button>
-
+                <button type="submit">댓글 등록</button><br>
                 <label>작성자: <input type="text"></label>
+
             </form>
             <div class="comment-list">
                 <ul>
@@ -54,17 +57,7 @@
                         <span class="comment-author">작성자: 한유림 | </span>
                         <span class="comment-date"> 작성일자: 2023.05.14</span>
                         <p class="comment-content">파이팅!</p>
-                    </li>
-                    <li>
-                        <span class="comment-author">작성자: 심유정 | </span>
-                        <span class="comment-date"> 작성일자: 2023.05.13</span>
-                        <p class="comment-content">메롱!</p>
-                    </li>
-                    <li>
-                        <span class="comment-author">작성자: 박정훈 | </span>
-                        <span class="comment-date"> 작성일자: 2023.05.12</span>
-                        <p class="comment-content">냥!</p>
-                    </li>
+                    </li>  
                 </ul>
             </div>
         </div>
