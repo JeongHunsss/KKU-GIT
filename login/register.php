7234117c1b70d4c9ccfab9e4fef3c24ae89016d4
roor <02tladbwjd@naver.com>
2023-05-22 15:59:59 +0900

    register 취소버튼 수정

--- login/register.php
+++ login/register.php
@@ -103,25 +103,6 @@ if (isset($_POST['code_check'])) {
     <meta charset="UTF-8">
     <link rel="stylesheet" type="text/css" href="./css/register.css">
 </head>
-<<<<<<< HEAD
-<body class="login-body">
-<form method="post" class="login-form">
-    <h2>KKU-GIT 회원가입</h2>
-    <label class="login-label" for="user_id">아이디:</label>
-    <input class="login-input" type="text" id="user_id" name="user_id" required>
-    <label class="login-label" for="password">비밀번호:</label>
-    <input class="login-input" type="password" id="password" name="password" required>
-    <label class="login-label" for="name">닉네임:</label>
-    <input class="login-input" type="text" id="name" name="name" required>
-    <button class="login-button" type="submit" name="register">회원가입</button>
-    <div><hr style="border-width: 1px; border-color: #ccc;"></div>
-<<<<<<< HEAD
-    <button class="cancel-button" onclick="javascript:history.back();">취소</button>
-=======
-    <button class="cancel-button" onclick="location.href='login.php'">취소</button>
->>>>>>> 9949747 (취소버튼 변경)
-</form>
-=======
 <body class="join-body">
     <form method="post" class="join-form">
         <h2>KKU-GIT 회원가입</h2>
@@ -185,6 +166,5 @@ if (isset($_POST['code_check'])) {
             <button class="duplicate-check-button" name="cancel">취소</button>
         </div>
     </form>
->>>>>>> 76537d27912d612d1ff05c3fd78892fa7c56e9bf
 </body>
 </html>
