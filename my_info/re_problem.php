ad140009a18ea6d59a78d3adb214d8aaccec39ba
JeongHunPark <mizz1030@naver.com>
2023-05-28 22:07:36 +0900

    0528 페이지네이션 및 기능 추가

--- my_info/re_problem.php
+++ my_info/re_problem.php
@@ -1,6 +1,20 @@
 <?php
     session_start();
     $_SESSION['cur_page'] = 'my_page';
+    $query_id = $_SESSION['user_id'];
+
+    include '../config/dbconfig.php';
+    include '../config/pagination_config.php';
+
+    // 등록된 문제의 총 개수 조회
+    $countQuery = "SELECT COUNT(*) AS total FROM problem_list WHERE author = '$query_id'";
+    $countResult = mysqli_query($conn, $countQuery);
+    $totalCount = mysqli_fetch_assoc($countResult)['total'];
+
+    // 등록된 문제 정보 가져오기
+    $regProInfoQuery = "SELECT * FROM problem_list WHERE author = '$query_id' LIMIT $startIndex, $resultsPerPage";
+    $regProInfoResult = mysqli_query($conn, $regProInfoQuery);
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -8,7 +22,9 @@
         <meta charset="UTF-8">
         <title>등록 문제 내역</title>
         <link rel="stylesheet" type="text/css" href="../bars/css/top_bar.css">
+        <link rel="stylesheet" type="text/css" href="../bars/css/pagination_bar.css">
         <link rel="stylesheet" type="text/css" href="./css/my_problem.css">
+        <link href="https://fonts.googleapis.com/css2?family=Jua&display=swap" rel="stylesheet">
     </head> 
     <?php include '../bars/top_bar.php'; ?>
     <body>
@@ -29,60 +45,24 @@
                     </tr>
                     </thead>
                     <tbody>
-                    <tr>
-                        <td>1</td> 
-                        <td>자료구조</td>
-                        <td>스택 구현하기</td>
-                        <td>홍길동</td>
-                        <td>2022-05-10</td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>알고리즘</td>
-                        <td>퀵 정렬 구현하기</td>
-                        <td>김철수</td>
-                        <td>2022-05-11</td>
-                    </tr>
-                    <tr>
-                        <td>3</td>
-                        <td>데이터베이스</td>
-                        <td>텀프 우리가 짱 먹음</td>
-                        <td>유 정</td>
-                        <td>2022-05-11</td>
-                    </tr>
-                    <tr>
-                        <td>3</td>
-                        <td>데이터베이스</td>
-                        <td>텀프 우리가 짱 먹음</td>
-                        <td>유 정</td>
-                        <td>2022-05-11</td>
-                    </tr> 
-                    <tr>
-                        <td>3</td>
-                        <td>데이터베이스</td>
-                        <td>텀프 우리가 짱 먹음</td>
-                        <td>유 정</td>
-                        <td>2022-05-11</td>
-                    </tr>
-                    <tr>
-                        <td>3</td>
-                        <td>데이터베이스</td>
-                        <td>텀프 우리가 짱 먹음</td>
-                        <td>유 정</td>
-                        <td>2022-05-11</td>
-                    </tr>
-                    <!-- 다른 문제들의 정보를 추가 -->
-
+                    <?php
+                        while ($row = mysqli_fetch_assoc($regProInfoResult)) {
+                            echo "<tr>";
+                            echo "<td>" . $row['idx'] . "</td>";
+                            echo "<td>" . $row['subject'] . "</td>";
+                            echo "<td>" . $row['title'] . "</td>";
+                            echo "<td>" . $row['author'] . "</td>";
+                            echo "<td>" . $row['date'] . "</td>";
+                            echo "</tr>";
+                        }
+                    ?>
                     </tbody>
                 </table>
                 </div>
-                <div class="pagination">
-                    <a href="#">&laquo;</a>
-                    <a href="#">1</a>
-                    <a href="#">2</a>
-                    <a href="#">3</a>
-                    <a href="#">&raquo;</a>
-                </div>
+                <!-- 페이지네이션 -->
+                <?php
+                    include '../bars/pagination_bar.php';
+                ?>
             
         </div>
 </body>
